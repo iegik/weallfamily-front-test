433cf59473941a283c993afa32f7112a93614c20
Янсон Артур <a.iansons@petrovich.ru>
2016-11-14 10:14:05 +0300

    Add rollup configuration

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,25 +1,30 @@
 import gulp from 'gulp'
-import sourcemaps from 'gulp-sourcemaps'
-import babel from 'gulp-babel'
-import concat from 'gulp-concat'
-import browserify from 'gulp-browserify'
+import sourcemaps from 'gulp-sourcemaps';
 import config from '../config'
+import rollup from 'rollup-stream'
+import babel from 'rollup-plugin-babel'
+import jsx from 'rollup-plugin-jsx'
+import buble from 'rollup-plugin-buble'
+
+import _interopRequireDefault from 'babel-cli/node_modules/babel-runtime/helpers/interop-require-default';
 
 const scripts = () =>
-    gulp.src(config.src, {
-            //base: 'src/components/app'
-            //base: '.'
-        })
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        // .pipe(gulp.dest(config.dest))
-        // .pipe(browserify({
-        //     insertGlobals : true,
-        //     debug : !!process.env.DEV
-        // }))
-        .pipe(concat(config.out))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(config.dest));
+gulp.src(config.src)
+//.pipe(sourcemaps.init())
+// transform the files here.
+    .pipe(rollup({
+        entry: config.entry,
+        dest: config.dest + '/' + config.out,
+        format: config.format,
+        //sourceMap: true,
+        plugins: [
+            //'transform-class-properties',
+            buble(),
+            jsx( {factory: 'React.createElement'} ),
+        ]
+    }))
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.dest))
 
 gulp.task('scripts', scripts);
 gulp.task('watch:scripts', () =>
